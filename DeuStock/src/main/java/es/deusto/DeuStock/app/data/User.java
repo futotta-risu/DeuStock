68a98ddc041aa82cf1ded6eb377cf1de440390cd
Lander San Millan <landersanmillan@opendeusto.es>
2021-03-19 12:48:45 +0100

    Class User

---
+++ DeuStock/src/main/java/es/deusto/DeuStock/app/data/User.java
@@ -0,0 +1,94 @@
+package es.deusto.DeuStock.app.data;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class User {
+	
+	String username;
+	String password;
+	String fullName;
+	Date birthDate;
+	String country;
+	String description;
+	Date registerDate;
+	Date lastActivity;
+	
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Date getRegisterDate() {
+		return registerDate;
+	}
+	public void setRegisterDate(Date registerDate) {
+		this.registerDate = registerDate;
+	}
+	public Date getLastActivity() {
+		return lastActivity;
+	}
+	public void setLastActivity(Date lastActivity) {
+		this.lastActivity = lastActivity;
+	}
+	
+	
+	public User(String username, String password, String fullName, Date birthDate, String country, String description) {
+		this.username = username;
+		this.password = password;
+		this.fullName = fullName;
+		this.birthDate = birthDate;
+		this.country = country;
+		this.description = description;
+		this.registerDate = Calendar.getInstance().getTime();
+		this.lastActivity = this.registerDate;
+	}
+	
+	public User() {
+		this.username = "NULL";
+		this.password = "NULL";
+		this.fullName = "NULL";
+		this.birthDate = Calendar.getInstance().getTime();
+		this.country = "NULL";
+		this.description = "NULL";
+		this.registerDate = Calendar.getInstance().getTime();
+		this.lastActivity = this.registerDate;
+	}
+	
+	
+	
+	
+
+}
