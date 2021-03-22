6473145f800598ff5ee0336b3f6eb57edfec218d
Lander San Millan <landersanmillan@opendeusto.es>
2021-03-22 16:37:29 +0100

    Main limpio

--- DeuStock/src/main/java/com/dekses/jersey/docker/demo/Main.java
+++ DeuStock/src/main/java/com/dekses/jersey/docker/demo/Main.java
@@ -4,16 +4,9 @@ import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
-import es.deusto.DeuStock.app.data.User;
-
 import java.io.IOException;
 import java.net.URI;
-import java.util.Date;
 
-import javax.jdo.JDOHelper;
-import javax.jdo.PersistenceManager;
-import javax.jdo.PersistenceManagerFactory;
-import javax.jdo.Transaction;
 
 /**
  * Main class.
@@ -43,32 +36,7 @@ public class Main {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-    	PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
 
-        System.out.println("DataNucleus AccessPlatform with JDO");
-        System.out.println("===================================");
-
-        // Persistence of a Product and a Book.
-        PersistenceManager pm = pmf.getPersistenceManager();
-        Transaction tx=pm.currentTransaction();
-        try
-        {
-            tx.begin();
-            System.out.println("Persisting products");
-            User user1 = new User("username", "password", "fullName", new Date(1234567890), "country", "description");
-            pm.makePersistent(user1);
- 
-            tx.commit();
-            System.out.println("Product and Book have been persisted");
-        }
-        finally
-        {
-            if (tx.isActive())
-            {
-                tx.rollback();
-            }
-            pm.close();
-        }
         final HttpServer server = startServer();
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
