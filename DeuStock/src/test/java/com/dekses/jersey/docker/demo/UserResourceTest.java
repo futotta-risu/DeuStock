96cf588d913cdc503491a5324509df14f7992b00
Lander San Millan <landersanmillan@opendeusto.es>
2021-03-31 12:25:00 +0200

    Añadidos JUnit REST login y register

---
+++ DeuStock/src/test/java/com/dekses/jersey/docker/demo/UserResourceTest.java
@@ -0,0 +1,73 @@
+package com.dekses.jersey.docker.demo;
+
+import static org.junit.Assert.*;
+
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import es.deusto.DeuStock.app.data.User;
+
+
+public class UserResourceTest {
+
+	
+    private HttpServer server;
+    private WebTarget target;
+
+    @Before
+    public void setUp() throws Exception {
+        // start the server
+        server = Main.startServer();
+        // create the client
+        Client c = ClientBuilder.newClient();
+
+        // uncomment the following line if you want to enable
+        // support for JSON in the client (you also have to uncomment
+        // dependency on jersey-media-json module in pom.xml and Main.startServer())
+        // --
+        //c.getConfiguration().enable(new org.glassfish.jersey.media.json.JsonJaxbFeature());
+
+        target = c.target(Main.BASE_URI);
+    }
+    
+	@SuppressWarnings("deprecation")
+	@After
+	public void tearDown() throws Exception {
+        server.stop();
+	}
+
+	
+	@Test
+	public void testRegister() {
+	
+		String jsonUser = "{\"birthDate\": \"1970-01-15T06:56:08Z[UTC]\",\"country\": \"SPAIN\",\"description\": \"TestAboutMe\",\"fullName\": \"TestFullName\",\"password\": \"TestPass\",\"username\": \"TestUser\"}";
+		
+		Response response = target.path("users").path("register").request().post(Entity.json(jsonUser));
+		assertEquals(Response.status(200).build().getStatus(), response.getStatus());
+		
+		Response responseCopy = target.path("users").path("register").request().post(Entity.json(jsonUser));
+		assertEquals(Response.status(401).build().getStatus(), responseCopy.getStatus());
+	}
+	
+	
+	@Test
+	public void testLogin() {
+        
+		User response = target.path("users").path("login").path("TestUser").path("TestPass").request().get(User.class);
+		
+        assertEquals("TestFullName", response.getFullName());
+		assertEquals("TestAboutMe", response.getDescription());
+	}
+	
+	
+
+}
