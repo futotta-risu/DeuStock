6e51b6ed9070891870765fe645051733d55aacb7
Lander San Millan <landersanmillan@opendeusto.es>
2021-04-01 15:30:01 +0200

    Añadida funcion REST DELETE User

--- DeuStock/src/main/java/com/dekses/jersey/docker/demo/UserResource.java
+++ DeuStock/src/main/java/com/dekses/jersey/docker/demo/UserResource.java
@@ -3,8 +3,10 @@ package com.dekses.jersey.docker.demo;
 
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -18,13 +20,13 @@ import es.deusto.DeuStock.app.data.User;
 @Path("/users")
 public class UserResource {
 	
-    @GET
+	@GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/login/{username}/{password}")
     public User login(@PathParam("username") String username,@PathParam("password") String password) {
-    	if(UserDAO.getUser(username) != null) {
-    		if(UserDAO.checkPassword(username, password)) {
-    			return(UserDAO.getUser(username));
+    	if(UserDAO.getInstance().getUser(username) != null) {
+    		if(UserDAO.getInstance().checkPassword(username, password)) {
+    			return(UserDAO.getInstance().getUser(username));
     		}
     	}
     	return null; 
@@ -35,9 +37,24 @@ public class UserResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("/register")
     public Response register(User user) {
-    	if(UserDAO.getUser(user.getUsername()) == null) {
-    		UserDAO.storeUser(user);
+    	if(UserDAO.getInstance().getUser(user.getUsername()) == null) {
+    		UserDAO.getInstance().storeUser(user);
     		return Response.status(200).build();
+    	}else {
+    		return Response.status(401).build();
+    	}
+    }
+    
+    @PUT
+    @Path("/delete/{username}/{password}")
+    public Response delete(@PathParam("username") String username, @PathParam("password") String password) {
+    	if(UserDAO.getInstance().getUser(username) != null) {
+    		if(UserDAO.getInstance().checkPassword(username, password)) {
+    			UserDAO.getInstance().deleteUser(UserDAO.getInstance().getUser(username));
+        		return Response.status(200).build();
+    		}else {
+        		return Response.status(401).build();
+    		}
     	}else {
     		return Response.status(401).build();
     	}
