41cb5b11e03481eb795340b26ffbac834302ebed
Amaia Acha <amaiaacha@opendeusto.es>
2021-04-02 23:27:18 +0200

    Revert "etiqueta de usuario hecha"
    This reverts commit b70117d2db2e8b3885a9a1ddc5d387db48220591.

--- DeustockClient/src/main/java/es/deusto/deustock/client/controllers/ControlButtonController.java
+++ DeustockClient/src/main/java/es/deusto/deustock/client/controllers/ControlButtonController.java
@@ -63,6 +63,5 @@ public class ControlButtonController implements DSGenericController{
     public void setParams(HashMap<String, Object> params) {
         if(params.containsKey("username"))
             this.username = String.valueOf(params.get("username"));
-        	perfilButton.setText(username);
     }
 }
