ac6133e47dfd843d5cebbee7f204dbd48f3f0cd5
Lander San Millan <landersanmillan@opendeusto.es>
2021-04-19 21:42:34 +0200

    Añadido Contiperf y Performance Test Yahoo API

--- DeuStock/src/test/java/es/deusto/deustock/dataminer/gateway/stocks/gateways/YahooFinanceGatewayPerformanceTest.java
+++ DeuStock/src/test/java/es/deusto/deustock/dataminer/gateway/stocks/gateways/YahooFinanceGatewayPerformanceTest.java
@@ -1,50 +1,53 @@
 package es.deusto.deustock.dataminer.gateway.stocks.gateways;
 
-import es.deusto.deustock.data.DeuStock;
 import es.deusto.deustock.dataminer.gateway.stocks.StockQueryData;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Tag;
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.Required;
+import org.databene.contiperf.junit.ContiPerfRule;
+import org.junit.Rule;
+import org.junit.*;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 /**
  * @author Erik B. Terres
  */
-@Tag("investment")
 public class YahooFinanceGatewayPerformanceTest {
-    
+	
+	@Rule
+	public ContiPerfRule i = new ContiPerfRule();
+
     @Test
+    @PerfTest(invocations = 50, threads = 5)
+    @Required(throughput = 10, average = 600, max = 2000, percentile95=1200)
     public void testGetStockListPerformanceSmall(){
-        long startTime = System.nanoTime();
         List<StockQueryData> stockList = new ArrayList<>();
         stockList.add(new StockQueryData("BB"));
         stockList.add(new StockQueryData("ETH"));
         stockList.add(new StockQueryData("AMZN"));
         stockList.add(new StockQueryData("GOG"));
 
-        HashMap<String, DeuStock> gateway = YahooFinanceGateway.getInstance().getStocksData(stockList);
-        long endTime = System.nanoTime();
-        System.out.println("Duration of small performance test " + (endTime - startTime)/1000000 );
+        YahooFinanceGateway.getInstance().getStocksData(stockList);
     }
+    
     @Test
+    @PerfTest(invocations = 50, threads = 5)
+    @Required(throughput = 10, average = 1200, max = 2500, percentile95=2000)
     public void testGetStockListPerformanceSmallAlternative(){
-        long startTime = System.nanoTime();
         List<StockQueryData> stockList = new ArrayList<>();
         stockList.add(new StockQueryData("PLTR"));
         stockList.add(new StockQueryData("AAPL"));
         stockList.add(new StockQueryData("NIO"));
         stockList.add(new StockQueryData("CIG"));
 
-        HashMap<String, DeuStock> gateway = YahooFinanceGateway.getInstance().getStocksData(stockList);
-        long endTime = System.nanoTime();
-        System.out.println("Duration of smalll alternative performance test " + (endTime - startTime)/1000000 );
+        YahooFinanceGateway.getInstance().getStocksData(stockList);
     }
 
     @Test
+    @PerfTest(invocations = 25, threads = 5)
+    @Required(throughput = 1, average = 3000, max = 5000, percentile95=3500)
     public void testGetStockListPerformanceBigThread() {
-        long startTime = System.nanoTime();
         List<StockQueryData> stockList = new ArrayList<>();
         stockList.add(new StockQueryData("CIG"));
         stockList.add(new StockQueryData("WFC"));
@@ -67,9 +70,7 @@ public class YahooFinanceGatewayPerformanceTest {
         stockList.add(new StockQueryData("AAL"));
         stockList.add(new StockQueryData("MSFT"));
 
-        HashMap<String, DeuStock> gateway = YahooFinanceGateway.getInstance().getStocksData(stockList);
-        long endTime = System.nanoTime();
-        System.out.println("Duration of big performance test " + (endTime - startTime)/1000000 );
+        YahooFinanceGateway.getInstance().getStocksData(stockList);
     }
 
 }
