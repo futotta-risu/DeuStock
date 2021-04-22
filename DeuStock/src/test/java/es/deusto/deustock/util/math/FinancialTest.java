66c2e6c33d01fec15161f1495404d9b27cd4c6b3
Erik Terres <37312201+erikberter@users.noreply.github.com>
2021-04-22 18:55:21 +0200

    Documented Report and added new math functionality

---
+++ DeuStock/src/test/java/es/deusto/deustock/util/math/FinancialTest.java
@@ -0,0 +1,57 @@
+package es.deusto.deustock.util.math;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Tag("math")
+class FinancialTest {
+
+    private List<Integer> numbers;
+
+    @BeforeEach
+    public void setUp(){
+        this.numbers = new ArrayList<>();
+
+        for(int i = 0; i < 20; i++){
+            numbers.add(i);
+        }
+        numbers.add(18);
+    }
+
+    @Test
+    public void rsiDouble(){
+
+
+        double result = Financial.rsi(this.numbers, 5);
+
+        assertTrue(result > 0.5);
+        assertEquals(result, 0.75, 0.01);
+    }
+
+    @Test
+    public void rsiFailsOnNegativeN(){
+        assertThrows(IllegalArgumentException.class, () -> Financial.rsi(this.numbers, -2));
+    }
+
+    @Test
+    public void rsiFailsOnZeroN(){
+        assertThrows(IllegalArgumentException.class, () -> Financial.rsi(this.numbers, 0));
+    }
+
+    @Test
+    public void rsiFailsOnOneN(){
+        assertThrows(IllegalArgumentException.class, () -> Financial.rsi(this.numbers, 1));
+    }
+
+    @Test
+    public void rsiFailsOnNBiggerThanListSize(){
+        assertThrows(IllegalArgumentException.class, () -> Financial.rsi(this.numbers, 200));
+    }
+
+}
