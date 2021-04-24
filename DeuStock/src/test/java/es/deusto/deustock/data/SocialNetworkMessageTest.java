08c90d378b8c8262415013e4634e92a66e22c8ba
Aritz Zugazaga <49875014+aritzugazaga@users.noreply.github.com>
2021-04-24 18:11:39 +0200

    Update SocialNetworkMessageTest.java

--- DeuStock/src/test/java/es/deusto/deustock/data/SocialNetworkMessageTest.java
+++ DeuStock/src/test/java/es/deusto/deustock/data/SocialNetworkMessageTest.java
@@ -7,8 +7,36 @@ import org.junit.Test;
 public class SocialNetworkMessageTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testMessage() {
+		SocialNetworkMessage socialNetworkMessage = new SocialNetworkMessage();
+		String testString = "Hola me gusta la CocaCola";
+		String expectedString = "Hola me gusta la CocaCola";
+		
+		socialNetworkMessage.setMessage(testString);
+		
+		assertEquals("getMessage and setMessage doesn't work", expectedString, socialNetworkMessage.getMessage());
+	}
+	
+	@Test
+	public void testAuthor() {
+		SocialNetworkMessage socialNetworkMessage = new SocialNetworkMessage();
+		String testString = "Aritz Zugazaga";
+		String expectedString = "Aritz Zugazaga";
+		
+		socialNetworkMessage.setAuthor(testString);
+		
+		assertEquals("getAuthor and setAuthor doesn't work", expectedString, socialNetworkMessage.getAuthor());
+	}
+	
+	@Test
+	public void testSentiment() {
+		SocialNetworkMessage socialNetworkMessage = new SocialNetworkMessage();
+		double testDouble = 12.52;
+		double expectedDouble = 12.52;
+		
+		socialNetworkMessage.setSentiment(testDouble);
+		
+		assertEquals("getSentiment and setSentiment doesn't work", expectedDouble, socialNetworkMessage.getSentiment());
 	}
 
 }
