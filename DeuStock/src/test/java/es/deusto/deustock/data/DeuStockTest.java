77c8fa285be4da5f680b0329f9c787408974f1fa
Aritz Zugazaga <49875014+aritzugazaga@users.noreply.github.com>
2021-04-24 17:59:28 +0200

    test de Clase Deustock acabada

--- DeuStock/src/test/java/es/deusto/deustock/data/DeuStockTest.java
+++ DeuStock/src/test/java/es/deusto/deustock/data/DeuStockTest.java
@@ -1,19 +1,73 @@
 package es.deusto.deustock.data;
 
-import java.math.BigDecimal;
-import static es.deusto.deustock.dataminer.cleaner.SocialTextCleaner.removeInvalidChars;
 import static org.junit.Assert.*;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
+
+import yahoofinance.histquotes.HistoricalQuote;
 
 public class DeuStockTest {
 
 	@Test
-	public void testGetPrice() {
-		BigDecimal testDecimal = 0.0000003;
-		BigDecimal expectedDecimal = 0.0000003;
-
-		assertEquals("The invalid chars remover doesn't work", expectedDecimal, removeInvalidChars(testDecimal));
+	public void testPrice() {
+		DeuStock deustock = new DeuStock();
+		BigDecimal testDecimal = new BigDecimal(50);
+		BigDecimal expectedDecimal = new BigDecimal(50);
+		
+		deustock.setPrice(testDecimal);
+		
+		assertEquals("getPrice and setPrice doesn't work", expectedDecimal, deustock.getPrice());
+	}
+	
+	@Test
+	public void testFullName() {
+		DeuStock deustock = new DeuStock();
+		String testString = "Aritz Zugazaga";
+		String expectedString = "Aritz Zugazaga";
+		
+		deustock.setFullName(testString);
+		
+		assertEquals("getFullName and setFullName doesn't work", expectedString, deustock.getFullName());
+	}
+	
+	@Test
+	public void testAcronym() {
+		DeuStock deustock = new DeuStock();
+		String testString = "AZ";
+		String expectedString = "AZ";
+		
+		deustock.setAcronym(testString);
+		
+		assertEquals("getAcronym and setAcronym doesn't work", expectedString, deustock.getAcronym());
 	}
 
+	@Test
+	public void testDescription() {
+		DeuStock deustock = new DeuStock();
+		String testString = "Hola me gusta la CocaCola";
+		String expectedString = "Hola me gusta la CocaCola";
+		
+		deustock.setDescription(testString);
+		
+		assertEquals("getDescription and setDescription doesn't work", expectedString, deustock.getDescription());
+	}
+	
+	@Test
+	public void testHistory() {
+		DeuStock deustock = new DeuStock();
+		List<HistoricalQuote> testList = new ArrayList<HistoricalQuote>();
+		List<HistoricalQuote> expectedList = new ArrayList<HistoricalQuote>();
+		HistoricalQuote HQ = new HistoricalQuote();
+		
+		HQ.setSymbol("BTC");
+		testList.add(HQ);
+		expectedList.add(HQ);
+		deustock.setHistory(testList);
+		
+		assertEquals("getHistory and setHistory doesn't work", expectedList, deustock.getHistory());
+	}
 }
---
+++ DeuStock/src/test/java/es/deusto/deustock/data/SocialNetworkMessageTest.java
@@ -0,0 +1,14 @@
+package es.deusto.deustock.data;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SocialNetworkMessageTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
