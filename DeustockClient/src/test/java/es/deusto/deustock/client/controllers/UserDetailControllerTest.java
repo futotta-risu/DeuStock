623c89d7938fb17bcebdbc5852d88f63417dd7c1
Erik Terres <37312201+erikberter@users.noreply.github.com>
2021-05-13 19:26:14 +0200

    Update UserDetailControllerTest.java

--- DeustockClient/src/test/java/es/deusto/deustock/client/controllers/UserDetailControllerTest.java
+++ DeustockClient/src/test/java/es/deusto/deustock/client/controllers/UserDetailControllerTest.java
@@ -1,43 +1,22 @@
 package es.deusto.deustock.client.controllers;
 
+import es.deusto.deustock.client.Main;
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import org.junit.BeforeClass;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.testfx.api.FxRobot;
-import org.testfx.assertions.api.Assertions;
-import org.testfx.framework.junit5.ApplicationExtension;
-import org.testfx.framework.junit5.Start;
-
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.testfx.api.FxRobot;
+import org.testfx.api.FxToolkit;
 
-import java.io.IOException;
 
-@ExtendWith(ApplicationExtension.class)
-public class UserDetailControllerTest{
+public class UserDetailControllerTest extends FxRobot{
 
-    @BeforeClass
-    public static void setupSpec() throws Exception {
-        if (Boolean.getBoolean("headless")) {
-            System.setProperty("testfx.robot", "glass");
-            System.setProperty("testfx.headless", "true");
-            System.setProperty("prism.order", "sw");
-            System.setProperty("prism.text", "t2k");
-            System.setProperty("java.awt.headless", "true");
-        }
-    }
-    /**
-     * Will be called with {@code @Before} semantics, i. e. before each test method.
-     *
-     * @param stage - Will be injected by the test runner.
-     */
-    @Start
-    private void start(Stage stage) throws IOException {
-        // set up the scene
+    @Test
+    public void testProgress() throws Exception {
+        // Setup JavaFX for testing.
+        Stage stage = FxToolkit.registerPrimaryStage();
+        Application app = FxToolkit.setupApplication(Main.class);
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/UserDetailView.fxml"));
         System.out.println(loader);
         Parent root = loader.load();
