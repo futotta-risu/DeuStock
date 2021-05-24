3851de0e7077c5f87ddf3bfa4ef6f37c5baa8820
Aritz Zugazaga <49875014+aritzugazaga@users.noreply.github.com>
2021-05-24 20:03:38 +0200

    Docu +

--- DeustockClient/src/main/java/es/deusto/deustock/client/visual/stocks/list/StockInfoSellLine.java
+++ DeustockClient/src/main/java/es/deusto/deustock/client/visual/stocks/list/StockInfoSellLine.java
@@ -10,6 +10,8 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 
 /**
+ * Line of information of a stock used for selling it in a list
+ * 
  * @author landersanmillan
  */
 public class StockInfoSellLine extends GridPane{
