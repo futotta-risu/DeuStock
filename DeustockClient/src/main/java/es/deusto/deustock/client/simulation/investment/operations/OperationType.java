af0ea254c217f22275ce1d5fe7887fe48a4ac8d8
Aritz Zugazaga <49875014+aritzugazaga@users.noreply.github.com>
2021-05-24 16:24:29 +0200

    Docuemtacion pero poquito

--- DeustockClient/src/main/java/es/deusto/deustock/client/simulation/investment/operations/OperationType.java
+++ DeustockClient/src/main/java/es/deusto/deustock/client/simulation/investment/operations/OperationType.java
@@ -1,6 +1,10 @@
 package es.deusto.deustock.client.simulation.investment.operations;
 
-
+/** Declaration of Enum OperationType
+ * 
+ *  The two types of different primitive data used for operate with stocks
+ *
+ */
 public enum OperationType {
     LONG, SHORT;
 }
